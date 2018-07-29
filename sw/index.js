8f4e3be64c63d3256a535c7b33606570ecff3cf8
Chase-Owens <chasejonathanowens@gmail.com>
2018-07-29 09:09:16 -0500

    Remove SW file

--- sw/index.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.register('/sw.js').then(function() {
-      console.log("Registered");
-    }).catch(function() {
-      console.log("Uh oh :(");
-    })
-  });
-}
